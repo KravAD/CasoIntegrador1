28b5d203023ce0e76969341499973c0b520ef760
KravAD <dkrav@myuax.com>
2024-02-21 11:51:29 +0100

    commit 7
    Implementación del código de la clase Cronometro Cósmico.

--- proyecto/src/main/java/org/example/CronometroCosmico.java
+++ proyecto/src/main/java/org/example/CronometroCosmico.java
@@ -1,4 +1,35 @@
 package org.example;
 
 public class CronometroCosmico {
-}
+
+        public static class Planet {
+            private double dayLength;
+            private double yearLength;
+
+            public Planet(double dayLength, double yearLength) {
+                this.dayLength = dayLength;
+                this.yearLength = yearLength;
+            }
+
+            public double[] convertTime(long earthSeconds) {
+                double planetSeconds = earthSeconds * this.dayLength / (24 * 60 * 60);
+                double planetMinutes = planetSeconds / 60;
+                double planetHours = planetMinutes / 60;
+                double planetDays = planetHours / this.dayLength;
+                double planetYears = planetDays / this.yearLength;
+
+                return new double[]{planetSeconds, planetMinutes, planetHours, planetDays, planetYears};
+            }
+
+            public long getIntLimit() {
+                return Long.MAX_VALUE;
+            }
+
+            public double getFloatLimit() {
+                return Double.MAX_VALUE;
+            }
+        }
+    }
+
+
+
--- proyecto/src/main/java/org/example/Main.java
+++ proyecto/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
+import org.example.CronometroCosmico.Planet;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+// Clase Cronometro Cosmico
+        Planet newPlanet = new Planet(30, 400);  // No necesitas especificar CronometroCosmico
+
+        long earthSeconds = 60 * 60 * 24 * 365;
+        double[] planetTime = newPlanet.convertTime(earthSeconds);
+        System.out.println("Time on new planet: " + planetTime[0] + " seconds, " + planetTime[1] + " minutes, " + planetTime[2] + " hours, " + planetTime[3] + " days, " + planetTime[4] + " years");
+
+        long intLimit = newPlanet.getIntLimit();
+        double floatLimit = newPlanet.getFloatLimit();
+        System.out.println("Int limit: " + intLimit + ", Float limit: " + floatLimit);
+
+// Clase Recursos y Suministros
     }
 }
