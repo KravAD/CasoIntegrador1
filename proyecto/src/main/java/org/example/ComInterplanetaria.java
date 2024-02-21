637bec39cbbf706df936b356e41f4779814aa128
KravAD <dkrav@myuax.com>
2024-02-21 12:19:19 +0100

    commit 16
    Código de la clase ComInterplanetaria.

--- proyecto/src/main/java/org/example/ComInterplanetaria.java
+++ proyecto/src/main/java/org/example/ComInterplanetaria.java
@@ -1,4 +1,25 @@
 package org.example;
 
+
 public class ComInterplanetaria {
+    public int contarVocales(String mensaje) {
+        int contador = 0;
+        String vocales = "aeiouAEIOU";
+        for (char c : mensaje.toCharArray()) {
+            if (vocales.indexOf(c) != -1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public String invertirMensaje(String mensaje) {
+        return new StringBuilder(mensaje).reverse().toString();
+    }
+
+    public boolean esPalindromo(String mensaje) {
+        String mensajeSinEspacios = mensaje.replace(" ", "");
+        String mensajeInvertido = invertirMensaje(mensajeSinEspacios);
+        return mensajeSinEspacios.equalsIgnoreCase(mensajeInvertido);
+    }
 }
